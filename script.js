c37d744e58b32adbcb9bcfc688c824eefb87dbec
jamescaples <53204570+jamescaples@users.noreply.github.com>
2019-10-05 23:22:08 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function generate(){
+var values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&*()";
+
+var values3 = "";
+
+
+}
+
+for (var i = 0; i<= i; i++) {
+password = values3 + values.charAt(math.floor(math.random()*math.floor(values.length -1)));
+}
+document.getElementById("display").values = password;
+
+document.getElementById ("length").innerHTML += 
+
+console.log(values3);
